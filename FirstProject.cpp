95ad04b91a709882b70094bfb35c19fc1f46e19f
HuseynovaDilara <huseynova.8885@gmail.com>
2023-08-13 15:13:16 +0400

    Project added

---
+++ FirstProject.cpp
@@ -0,0 +1,50 @@
+﻿// FirstProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{   // Task 1
+
+    cout << " * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * " << '\n';
+    cout << " * \\n - new line              * \\a - sound in console          *" << '\n';
+    cout << " * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *" << '\n';
+    cout << " * \\t - tabulation  * \\r - caret to the beginning of the line  *" << '\n';
+    cout << " * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *" << '\n';
+    cout << " * \\n - new line              * \\b - carriage return back      * " << '\n';
+    cout << " * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *" << '\n';
+    // Task 2 Выведите на экран ёлочку:
+
+    cout << "       /\\ " << '\n';
+    cout << "      /*\\\\ " << '\n';
+    cout << "     ///\\*\\ " << '\n';
+    cout << "       ||" << '\n';
+    cout << "    /*//\\\\*\\ " << '\n';
+    cout << "   //*//\\\\*\\\\" << '\n';
+    cout << "       ||" << '\n';
+    cout << "  /*////\\\\\\\\*\\\ " << '\n';
+    cout << " /*///*/\\*\\\\\\*\\\ " << '\n';
+    cout << "       ||" << '\n'<< '\n';
+
+    // солнышко
+
+    cout << "     \\ | | /" << '\n';
+    cout << "    \\ * * * /" << '\n';
+    cout << "  -- *\\\\|//* --" << '\n';
+    cout << "  -- *//|\\\\* --  " << '\n';
+    cout << "    / * * * \\\ " << '\n';
+    cout << "     / | | \\\ " << '\n' << '\n';
+
+    // Task 2* Выведите на экран удивлённую звёздочку:
+
+    cout << "        . " << '\n';
+    cout << "  \\     ,     /" << '\n';
+    cout << " ' ,___/_\\\___, '" << '\n';
+    cout << "    \\\ /o.o\\\ /" << '\n';
+    cout << "-=   > \\\_/ <   =-" << '\n';
+    cout << "    /_\\\___/_\\\ " << '\n';
+    cout << " . `   \\\ /   ` ." << '\n';
+    cout << "     /  `  \\\ " << '\n';
+    cout << "        . ";
+}
